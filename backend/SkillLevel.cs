fb8a2f70b687aedac61be2897ed498fefd520950
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-09 00:36:39 +0100

    add skilllevel wrapper do enable multiple ratings

---
+++ backend/SkillLevel.cs
@@ -0,0 +1,35 @@
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using backend.Rating;
+
+namespace backend
+{
+    public class SkillLevel : IComparable
+    {        
+        private readonly IList<IRating> myRatings;
+
+        public double SkillScore => myRatings.Average(x => x.Score);
+        public SkillLevel()
+        {
+            myRatings = new List<IRating>();
+        }
+
+        public void AddRating(IRating rating)
+        {
+            if (!myRatings.Contains(rating))
+            {
+                myRatings.Add(rating);
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            var score = (obj as SkillLevel).SkillScore;
+
+            return score < SkillScore ? -1 : score == SkillScore ? 0 : 1;
+        }
+    }
+}
