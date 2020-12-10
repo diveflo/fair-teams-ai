013f3be0857d62a11e8348dd02a3faa15c280720
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-10 07:43:17 +0100

    add wrapper class for both teams

---
+++ backend/Assignment.cs
@@ -0,0 +1,15 @@
+namespace backend
+{
+    public class Assignment
+    {
+        public Team Terrorists { get; }
+
+        public Team CounterTerrorists { get; }
+
+        public Assignment(Team terrorists, Team counterTerrorists)
+        {
+            Terrorists = terrorists;
+            CounterTerrorists = counterTerrorists;
+        }
+    }
+}
