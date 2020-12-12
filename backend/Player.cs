bbdd2c01a920d8e0e97a33c9600f4ffc57d90631
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-12 19:38:22 +0100

    cleanup code formatting

--- backend/Player.cs
+++ backend/Player.cs
@@ -9,9 +9,7 @@ namespace backend
     {
         private const string steamAPIKey = "B0E3E0ED2572C01223E0ED7043E9678C";
         public string Name { get; set; }
-
         public string SteamID { get; set; }
-
         public SkillLevel Skill { get; set; }
 
         public void ScrapeName()
@@ -29,21 +27,17 @@ namespace backend
 
             webRequest.ContentType = "application/json";
 
-            using (var s = webRequest.GetResponse().GetResponseStream())
+            using var responseStream = webRequest.GetResponse().GetResponseStream();
+            using var responseStreamReader = new StreamReader(responseStream);
+            dynamic responseJson = JsonConvert.DeserializeObject(responseStreamReader.ReadToEnd());
+
+            if (responseJson.response.players.Count != 1)
             {
-                using (var sr = new StreamReader(s))
-                {
-                    dynamic json = JsonConvert.DeserializeObject(sr.ReadToEnd());
-
-                    if (json.response.players.Count != 1)
-                    {
-                        throw new ArgumentException($"The SteamID ({SteamID}) you provided could not be found. Please check the player's Steam community profile URL.");
-                    }
-
-                    string profileName = json.response.players[0].personaname;
-                    Name = profileName.Replace("'", "");
-                }
+                throw new ArgumentException($"The SteamID ({SteamID}) you provided could not be found. Please check the player's Steam community profile URL.");
             }
+
+            string profileName = responseJson.response.players[0].personaname;
+            Name = profileName.Replace("'", "");
         }
     }
 }
