2c906c29ec371e086bd7fc21b6f1861df9caa2db
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-12 02:05:24 +0100

    enable swagger also in production environments to simplify API doc

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -30,12 +30,9 @@ namespace backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "fair-teams-ai v1"));
 
             app.UseHttpsRedirection();
 
