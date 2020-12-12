49e2bfc6013047802599a9aa22bdb70e5e0ef17e
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-12 02:05:46 +0100

    align swagger title with project name

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -23,7 +23,7 @@ namespace backend
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "fair-teams-ai api", Version = "v1" });
             });
         }
 
