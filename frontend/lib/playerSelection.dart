11b5724c1838e0dd38503a8f4443507ffb35891b
Michael Sandner <Michael.Sandner@methodpark.de>
2020-12-13 11:11:25 +0100

    Add player input

--- frontend/lib/playerSelection.dart
+++ frontend/lib/playerSelection.dart
@@ -81,7 +81,7 @@ class _PlayerSelectionState extends State<PlayerSelection> {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         SizedBox(
-          height: 30,
+          height: 20,
         ),
         Expanded(
           flex: 2,
