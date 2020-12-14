42b230db7fdecce3215345fa5582dceff4e2ce0a
Florian Gabsteiger <florian.g@me.com>
2020-12-15 00:24:18 +0100

    rating cleanups (#38)

--- backend/Rating/HLTVRating.cs
+++ backend/Rating/HLTVRating.cs
@@ -4,12 +4,6 @@ namespace backend.Rating
     public class HLTVRating : IRating
     {
         public string Name => "HLTV";
-        public double Score { get; set; }
-
-        public void ScrapeForPlayer()
-        {
-            var random = new Random();
-            Score = random.NextDouble();
-        }
+        public double Score { get; }
     }
 }
