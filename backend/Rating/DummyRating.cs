3769074fb2c170e0129698617500356ac680ef3d
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-15 00:23:44 +0100

    rating cleanups

--- backend/Rating/DummyRating.cs
+++ backend/Rating/DummyRating.cs
@@ -2,7 +2,7 @@ namespace backend.Rating
 {
     public class DummyRating : IRating
     {
-        public string Name => "HLTV";
+        public string Name => "Dummy";
         public double Score { get; set; }
     }
 }
