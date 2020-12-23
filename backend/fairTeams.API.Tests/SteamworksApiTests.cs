6f9d7722139366c1435f9bab301e516aa3111f90
Florian Gabsteiger <florian.g@me.com>
2020-12-23 21:46:48 +0100

    Backend/steamapitests (#52)
    * Use new dummy steam account for unit test
    
    The dummy steam account has a fixed username ("fairteamsai") and won't change it in the future.
    This should make our unit tests more stable rather than using one of our accounts.
    
    * Ensure non-public profiles are handeled correctly
    
    The used dummy steam account has their game details set to private/friends only.
    
    * Ensure a valid and public steam account (mine) returns (the most important) user statistics

--- backend/fairTeams.API.Tests/SteamworksApiTests.cs
+++ backend/fairTeams.API.Tests/SteamworksApiTests.cs
@@ -8,16 +8,18 @@ namespace fairTeams.API.Tests
 {
     public class SteamworksApiTests
     {
+        private const string myPrivateProfilePlayerSteamID = "76561199120831930";
+        private const string myPrivateProfilePlayerSteamUsername = "fairteamsai";
+        private const string myPublicProfilePlayerSteamID = "76561197973591119";
+
         [Fact]
-        public void ParseSteamUsernames_FloSteamID_ReturnsAntifasupersoldier()
+        public void ParseSteamUsernames_ValidSteamID_ReturnsFairTeamsAIUsername()
         {
-            var expectedSteamUsername = "antifa super soldier";
-            var floSteamID = "76561197973591119";
-            var flo = new Player { Name = "Flo", SteamID = floSteamID };
+            var user = new Player { SteamID = myPrivateProfilePlayerSteamID };
 
-            flo = SteamworksApi.SteamworksApi.ParseSteamUsernames(new List<Player> { flo }).Result.First();
+            user = SteamworksApi.SteamworksApi.ParseSteamUsernames(new List<Player> { user }).Result.First();
 
-            Assert.Equal(expectedSteamUsername, flo.SteamName);
+            Assert.Equal(myPrivateProfilePlayerSteamUsername, user.SteamName);
         }
 
         [Fact]
@@ -29,6 +31,35 @@ namespace fairTeams.API.Tests
             var parseUsernameTask = SteamworksApi.SteamworksApi.ParseSteamUsernames(new List<Player> { nonExistingUser });
 
             await Assert.ThrowsAsync<PlayerNotFoundException>(() => parseUsernameTask);
+        }
+
+        [Fact]
+        public async Task ParsePlayerStatistics_ProfileNotPublic_ThrowsProfileNotPublicException()
+        {
+            var parseStatisticsTask = SteamworksApi.SteamworksApi.ParsePlayerStatistics(myPrivateProfilePlayerSteamID);
+
+            await Assert.ThrowsAsync<ProfileNotPublicException>(() => parseStatisticsTask);
+        }
+
+        [Fact]
+        public void ParsePlayerStatistics_ProfilePublic_ReturnsExpectedStatistics()
+        {
+            var expectedStatistics = new List<string>
+            {
+                "total_kills",
+                "total_deaths",
+                "total_damage_done",
+                "total_kills_bizon"
+            };
+
+            var statistics = SteamworksApi.SteamworksApi.ParsePlayerStatistics(myPublicProfilePlayerSteamID).Result;
+
+            Assert.True(statistics.Any());
+
+            foreach(var expectedStatistic in expectedStatistics)
+            {
+                Assert.True(statistics.Select(x => x.Name == expectedStatistic).Any());
+            }
         }
     }
 }
