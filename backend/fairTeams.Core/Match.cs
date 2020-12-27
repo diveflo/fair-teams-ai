05d9590a8d7c15c6062e37959d3ead5719d6f631
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-27 16:49:36 +0100

    Extract MatchPlayer into own type

--- backend/fairTeams.Core/Match.cs
+++ backend/fairTeams.Core/Match.cs
@@ -3,27 +3,6 @@ using System.Collections.Generic;
 
 namespace fairTeams.Core
 {
-    public class MatchPlayer : IEquatable<MatchPlayer>
-    {
-        public long SteamID { get; set; }
-        public string Name { get; set; }
-
-        public bool Equals(MatchPlayer other)
-        {
-            return SteamID == other.SteamID;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as MatchPlayer);
-        }
-
-        public override int GetHashCode()
-        {
-            return SteamID.GetHashCode();
-        }
-    }
-
     public class Match
     {
         public Demo Demo { get; set; }
---
+++ backend/fairTeams.Core/MatchPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fairTeams.Core
+{
+    public class MatchPlayer : IEquatable<MatchPlayer>
+    {
+        public long SteamID { get; set; }
+        public string Name { get; set; }
+
+        public bool Equals(MatchPlayer other)
+        {
+            return SteamID == other.SteamID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as MatchPlayer);
+        }
+
+        public override int GetHashCode()
+        {
+            return SteamID.GetHashCode();
+        }
+    }
+}
