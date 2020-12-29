690f5c0d9443386a779248cb23b8d5429b70366e
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-29 18:47:02 +0100

    Add bz2 downloading and extracting

---
+++ backend/fairTeams.DemoHandling.Tests/DemoDownloaderTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace fairTeams.DemoHandling.Tests
+{
+    public class DemoDownloaderTests
+    {
+        [Fact]
+        public void DownloadDemoArchive_SmallTestFile_SuccessfullyDownloaded()
+        {
+            var downloadUrl = @"https://fairteamsai.backend.files.entertainment720.eu/test.txt.bz2";
+            var expectedHash = new byte[] { 148, 155, 249, 87, 238, 163, 193, 104, 89, 218, 236, 53, 69, 241, 46, 123 };
+
+            var locallyDownloadedDemoFile = DemoDownloader.DownloadDemoArchive(downloadUrl);
+
+            using var stream = File.OpenRead(locallyDownloadedDemoFile);
+            var downloadedFileHash = MD5.Create().ComputeHash(stream);
+
+            Assert.Equal(expectedHash, downloadedFileHash);
+        }
+
+        [Fact]
+        public async Task DecompressDemoArchive_SmallTestFile_SuccessfullyDecompressedAsync()
+        {
+            var downloadUrl = @"https://fairteamsai.backend.files.entertainment720.eu/test.txt.bz2";
+            var locallyDownloadedDemoFile = DemoDownloader.DownloadDemoArchive(downloadUrl);
+
+            var decompressedFile = await DemoDownloader.DecompressDemoArchive(locallyDownloadedDemoFile);
+
+            var content = File.ReadAllText(decompressedFile);
+            Assert.Equal("hi", content);
+        }
+    }
+}
---
+++ backend/fairTeams.DemoHandling/DemoDownloader.cs
@@ -0,0 +1,46 @@
+﻿using ICSharpCode.SharpZipLib.BZip2;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace fairTeams.DemoHandling
+{
+    public class DemoDownloader
+    {
+        public static string DownloadDemoArchive(string downloadUrl)
+        {
+            var realFileExtension = GetRealFileExtensionFromBZip2DownloadURL(downloadUrl);
+
+            var downloadLocation = Path.GetTempFileName().Replace(".tmp", realFileExtension + ".bz2");
+            using var webClient = new WebClient();
+            webClient.DownloadFile(downloadUrl, downloadLocation);
+            return downloadLocation;
+        }
+
+        public static async Task<string> DecompressDemoArchive(string bz2FilePath)
+        {
+            if (!File.Exists(bz2FilePath))
+            {
+                throw new ArgumentException($"The archive file {bz2FilePath} does not exist.");
+            }
+
+            var taskCompletitionSource = new TaskCompletionSource<string>();
+            string destination = bz2FilePath.Replace(".bz2", "");
+
+            return await Task.Run(() =>
+            {
+                 BZip2.Decompress(File.OpenRead(bz2FilePath), File.Create(destination), true);
+                return destination;
+            });
+        }
+
+        private static string GetRealFileExtensionFromBZip2DownloadURL(string url)
+        {
+            var urlWithoutBZip2Extension = url[0..^4];
+            var indexOfRealFileExtension = urlWithoutBZip2Extension.LastIndexOf(".");
+            var lengthOfRealFileExtension = urlWithoutBZip2Extension.Length - indexOfRealFileExtension;
+            return urlWithoutBZip2Extension.Substring(indexOfRealFileExtension, lengthOfRealFileExtension);
+        }
+    }
+}
