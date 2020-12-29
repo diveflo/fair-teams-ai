ab7fd6169be92f3eb05a74b9c6d84af8adeecc65
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-29 18:47:35 +0100

    Add Match share code decoder/handling

---
+++ backend/fairTeams.DemoHandling.Tests/ShareCodeTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace fairTeams.DemoHandling.Tests
+{
+    public class ShareCodeTests
+    {
+        [Fact]
+        public void Decode_ValidShareCode_ReturnsValidMatchId1()
+        {
+            var shareCode = "CSGO-JyGRk-R6F42-BNsVL-UaMAV-QmeqA";
+
+            var gameRequest = ShareCode.Decode(shareCode);
+
+            Assert.Equal(3455336489968206603u, gameRequest.MatchId);
+            Assert.Equal(3455341431328080096u, gameRequest.OutcomeId);
+            Assert.Equal(2615u, gameRequest.Token);
+        }
+    }
+}
