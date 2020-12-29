cba4019ea0d31f71b511009c055e19554fa20458
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-29 03:43:34 +0100

    Provide functional steam account in preparation of demo downloader
    To get the demo file download url for a given match (sharing code), the steam sdk requires a running and logged in steam client.
    As the CSGO game coordinator, which provides this information, requires a valid steam account which ownes CS:GO, we created a functional steam acount for this purpose.
    The steam account credentials are read from two environment variables and then used inside our code base.
    To allow our automated CIs to run, these credentials have been added as GitHub secrets and are then added as environment variables during CI runs.

---
+++ backend/fairTeams.Core/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace fairTeams.Core
+{
+    public static class Settings
+    {
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+        public static string SteamUsername = System.Environment.GetEnvironmentVariable("STEAM_USER");
+        public static string SteamPassword = System.Environment.GetEnvironmentVariable("STEAM_PASSWORD");
+#pragma warning restore CA2211 // Non-constant fields should not be visible
+    }
+}
