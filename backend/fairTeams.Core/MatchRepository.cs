a12deab9e45df8169dab87e9b234eae36b4669b6
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2020-12-30 16:23:57 +0100

    Add persistency for Matches via Entity Framework

---
+++ backend/fairTeams.Core/MatchRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fairTeams.Core
+{
+    public class MatchRepository : DbContext
+    {
+        public DbSet<Match> Matches { get; set; }
+
+        public MatchRepository(DbContextOptions<MatchRepository> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.Demo);
+            modelBuilder.Entity<Match>()
+                .HasKey(m => m.Id);
+            modelBuilder.Entity<Match>()
+                .HasMany(m => m.PlayerResults);
+        }
+    }
+}
