8b0599f075fb77940d16a654933fc98ab17e131b
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-01 03:24:55 +0100

    Persistence for HNYB members and their auth tokens to prepare for automated demo downloading

---
+++ backend/fairTeams.DemoHandling/MatchMakingSteamUser.cs
@@ -0,0 +1,9 @@
+﻿namespace fairTeams.DemoHandling
+{
+    public class MatchMakingSteamUser
+    {
+        public long SteamID { get; set; }
+        public string AuthenticationCode { get; set; }
+        public string LastSharingCode { get; set; }
+    }
+}
---
+++ backend/fairTeams.DemoHandling/SteamUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace fairTeams.DemoHandling
+{
+    public class SteamUserRepository : DbContext
+    {
+        public DbSet<MatchMakingSteamUser> SteamUsers { get; set; }
+
+        public SteamUserRepository(DbContextOptions<SteamUserRepository> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MatchMakingSteamUser>()
+                .HasKey(m => m.SteamID);
+        }
+    }
+}
