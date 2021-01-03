5599ee2e00429cb15a20e22c3d7799308187bc6f
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-03 06:00:53 +0100

    Update utests

--- backend/fairTeams.DemoHandling.Tests/GameCoordinatorTests.cs
+++ backend/fairTeams.DemoHandling.Tests/GameCoordinatorTests.cs
@@ -9,17 +9,17 @@ namespace fairTeams.DemoHandling.Tests
         /// This will fail pretty soon once Valve expires the demo...we should probably instead somehow get a current/recent share-code automatically.
         /// Though this kind of breaks the whole "unit"-test approach :/
         /// </summary>
-        [Fact]
+        [Fact(Skip = "See comment above. This will fail soon...")]
         public void GetMatchInfo_ShareCodeInput_ReturnsCorrectDateAndDownloadURL()
         {
             var gameCoordinatorClient = new GameCoordinatorClient();
-            var gameRequest = ShareCode.Decode("CSGO-inhFT-GJ96p-bkn6N-duydj-a5UrC");
+            var gameRequest = ShareCode.Decode("CSGO-j6hrT-hvqmd-pNMXY-TuTrq-aXnMC");
             var demo = new Demo { GameRequest = gameRequest };
 
             var match = gameCoordinatorClient.GetMatchInfo(demo);
 
-            var expectedMatchDate = new System.DateTime(637438359890000000);
-            var expectedDownloadURL = @"http://replay191.valve.net/730/003453673016922210752_2051865056.dem.bz2";
+            var expectedMatchDate = new System.DateTime(637451358060000000);
+            var expectedDownloadURL = @"http://replay191.valve.net/730/003456465718474703287_0558788749.dem.bz2";
 
             Assert.Equal(expectedMatchDate, match.Date);
             Assert.Equal(expectedDownloadURL, match.Demo.DownloadURL);
