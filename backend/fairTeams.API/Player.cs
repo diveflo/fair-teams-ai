25c3c4b7736ee3b00e26f1175c205fd1e8141816
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-05 02:52:06 +0100

    Add RequestPlayer class for new (reduced) rest api input

--- backend/fairTeams.API/Player.cs
+++ backend/fairTeams.API/Player.cs
@@ -1,11 +1,30 @@
+using System;
+
 namespace fairTeams.API
 {
     public class Player
     {
         public string Name { get; set; }
         public string SteamName { get; set; }
+        [Obsolete("Please do not use anymore.")]
         public bool ProfilePublic { get; set; }
         public string SteamID { get; set; }
         public SkillLevel Skill { get; set; }
+
+        public Player() { }
+
+        public Player(RequestPlayer obj)
+        {
+            Name = obj.Name;
+            SteamName = obj.SteamName;
+            SteamID = obj.SteamID;
+        }
+    }
+
+    public class RequestPlayer
+    {
+        public string Name { get; set; }
+        public string SteamName { get; set; }
+        public string SteamID { get; set; }
     }
 }
