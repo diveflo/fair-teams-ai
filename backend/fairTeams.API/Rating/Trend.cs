44771e115bfd83449cc1d5d7bcc3bdf1b2a0f4f3
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-10 05:07:18 +0100

    Add new Trend enum

---
+++ backend/fairTeams.API/Rating/Trend.cs
@@ -0,0 +1,29 @@
+﻿namespace fairTeams.API.Rating
+{
+    public enum Trend
+    {
+        Plateau,
+        Upwards,
+        Downwards
+    }
+
+    public static class TrendHelper
+    {
+        public static Trend GetTrend(double overallScore, double latestScore)
+        {
+            const double plateauDelta = 0.05d;
+
+            if ((overallScore + plateauDelta) < latestScore)
+            {
+                return Trend.Upwards;
+            }
+
+            if ((overallScore - plateauDelta) > latestScore)
+            {
+                return Trend.Downwards;
+            }
+
+            return Trend.Plateau;
+        }
+    }
+}
