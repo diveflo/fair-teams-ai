adb16e8802cdfc844549b96e0690eabe4a97ebf2
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-10 05:09:15 +0100

    Implement trending for KDRating

--- backend/fairTeams.API.Tests/KDRatingTests.cs
+++ backend/fairTeams.API.Tests/KDRatingTests.cs
@@ -12,9 +12,11 @@ namespace fairTeams.API.Tests
         public void Score_EqualNumberOfKillsAndDeaths_ReturnsOne()
         {
             var kills = new Statistic { Name = "total_kills", Value = 10 };
+            var lastMatchKills = new Statistic { Name = "last_match_kills", Value = 0 };
             var deaths = new Statistic { Name = "total_deaths", Value = 10 };
+            var lastMatchDeaths = new Statistic { Name = "last_match_deaths", Value = 1 };
 
-            var kdRating = new KDRating(new List<Statistic>() { kills, deaths });
+            var kdRating = new KDRating(new List<Statistic>() { kills, lastMatchKills, deaths, lastMatchDeaths });
 
             Assert.Equal(1.0d, kdRating.Score);
         }
@@ -23,9 +25,11 @@ namespace fairTeams.API.Tests
         public void Score_TwiceAsManyKillsAsDeaths_ReturnsTwo()
         {
             var kills = new Statistic { Name = "total_kills", Value = 20 };
+            var lastMatchKills = new Statistic { Name = "last_match_kills", Value = 0 };
             var deaths = new Statistic { Name = "total_deaths", Value = 10 };
+            var lastMatchDeaths = new Statistic { Name = "last_match_deaths", Value = 1 };
 
-            var kdRating = new KDRating(new List<Statistic>() { kills, deaths });
+            var kdRating = new KDRating(new List<Statistic>() { kills, lastMatchKills, deaths, lastMatchDeaths });
 
             Assert.Equal(2.0d, kdRating.Score);
         }
@@ -40,6 +44,18 @@ namespace fairTeams.API.Tests
         }
 
         [Fact]
+        public void Score_MissingLastMatchKillsStatistic_ThrowsArgumentException()
+        {
+            var kills = new Statistic { Name = "total_kills", Value = 10 };
+            var deaths = new Statistic { Name = "total_deaths", Value = 10 };
+            var lastMatchDeaths = new Statistic { Name = "last_match_deaths", Value = 10 };
+
+            var statistics = new List<Statistic>() { kills, deaths, lastMatchDeaths };
+
+            Assert.Throws<ArgumentException>(() => new KDRating(statistics));
+        }
+
+        [Fact]
         public void Score_MissingDeathsStatistic_ThrowsArgumentException()
         {
             var kills = new Statistic { Name = "total_kills", Value = 20 };
@@ -49,11 +65,26 @@ namespace fairTeams.API.Tests
         }
 
         [Fact]
+        public void Score_MissingLastMatchDeathsStatistic_ThrowsArgumentException()
+        {
+            var kills = new Statistic { Name = "total_kills", Value = 10 };
+            var deaths = new Statistic { Name = "total_deaths", Value = 10 };
+            var lastMatchKills = new Statistic { Name = "last_match_kills", Value = 10 };
+
+            var statistics = new List<Statistic>() { kills, deaths, lastMatchKills };
+
+            Assert.Throws<ArgumentException>(() => new KDRating(statistics));
+        }
+
+        [Fact]
         public void Score_ZeroDeaths_ReturnsInfinity()
         {
             var kills = new Statistic { Name = "total_kills", Value = 20 };
+            var lastMatchKills = new Statistic { Name = "last_match_kills", Value = 0 };
             var deaths = new Statistic { Name = "total_deaths", Value = 0 };
-            var kdRating = new KDRating(new List<Statistic>() { kills, deaths });
+            var lastMatchDeaths = new Statistic { Name = "last_match_deaths", Value = 1 };
+
+            var kdRating = new KDRating(new List<Statistic>() { kills, lastMatchKills, deaths, lastMatchDeaths });
 
             Assert.Equal(double.PositiveInfinity, kdRating.Score);
         }
--- backend/fairTeams.API/Rating/KDRating.cs
+++ backend/fairTeams.API/Rating/KDRating.cs
@@ -14,15 +14,30 @@ namespace fairTeams.API.Rating
 
         public KDRating(IEnumerable<Statistic> playerStatistics)
         {
-            if (!playerStatistics.Any(x => x.Name == "total_kills") || !playerStatistics.Any(x => x.Name == "total_deaths"))
-            {
-                throw new ArgumentException("The player statistics have to contain 'total_kills' and 'total_deaths' to compute the KD rating.");
-            }
+            CheckRequiredStatistics(playerStatistics);
 
             var kills = playerStatistics.Single(x => x.Name == "total_kills").Value;
             var deaths = playerStatistics.Single(x => x.Name == "total_deaths").Value;
-
             Score = (double)kills / deaths;
+
+            var lastMatchKills = playerStatistics.Single(x => x.Name == "last_match_kills").Value;
+            var lastMatchDeaths = playerStatistics.Single(x => x.Name == "last_match_deaths").Value;
+            var lastMatchKD = (double)lastMatchKills / lastMatchDeaths;
+
+            Trend = TrendHelper.GetTrend(Score, lastMatchKD);
+        }
+
+        private static void CheckRequiredStatistics(IEnumerable<Statistic> playerStatistics)
+        {
+            var requiredStatistics = new List<string> { "total_kills", "last_match_kills", "total_deaths", "last_match_deaths" };
+
+            foreach (var requiredStatistic in requiredStatistics)
+            {
+                if (!playerStatistics.Any(x => x.Name.Equals(requiredStatistic)))
+                {
+                    throw new ArgumentException($"The player statistics didn't contain the required statistic {requiredStatistic}");
+                }
+            }
         }
     }
 }
