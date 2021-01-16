034752cc32087ce1fb6fa1265a821d79f6d036ba
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-01-16 15:05:27 +0100

    Add Rank to persistency layer

---
+++ backend/fairTeams.API/Migrations/SteamUserRepository/20210116000930_AddRank.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fairTeams.API.Migrations.SteamUserRepository
+{
+    public partial class AddRank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "SteamUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "SteamUsers");
+        }
+    }
+}
--- backend/fairTeams.DemoHandling/MatchMakingSteamUser.cs
+++ backend/fairTeams.DemoHandling/MatchMakingSteamUser.cs
@@ -1,8 +1,11 @@
-﻿namespace fairTeams.DemoHandling
+﻿using fairTeams.Core;
+
+namespace fairTeams.DemoHandling
 {
     public class MatchMakingSteamUser
     {
         public long SteamID { get; set; }
+        public Rank Rank { get; set; }
         public string AuthenticationCode { get; set; }
         public string LastSharingCode { get; set; }
     }
