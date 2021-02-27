337eee5c5c1b77190f8b531b7671a5cfc7a09391
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-02-27 19:36:28 +0100

    Demo backup to an FTP server

---
+++ backend/fairTeams.DemoHandling/DemoBackuper.cs
@@ -0,0 +1,71 @@
+﻿using fairTeams.Core;
+using FluentFTP;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+
+namespace fairTeams.DemoHandling
+{
+    public sealed class DemoBackuper : IDisposable
+    {
+        private readonly ILogger<DemoBackuper> myLogger;
+        private readonly FtpClient myFtpClient;
+
+        public DemoBackuper(ILogger<DemoBackuper> logger)
+        {
+            myLogger = logger;
+
+            var ftpCredentials = new NetworkCredential(Settings.DemoBackupFTPUsername, Settings.DemoBackupFTPPassword);
+            myFtpClient = new FtpClient(Settings.DemoBackupFTP, 1818, ftpCredentials);
+            myFtpClient.AutoConnect();
+        }
+
+        public DemoBackuper() : this(UnitTestLoggerCreator.CreateUnitTestLogger<DemoBackuper>()) { }
+
+        public void BackupDemoFile(Demo demo)
+        {
+            BackupDemoFile(demo, false);
+        }
+
+        public void BackupDemoFile(Demo demo, bool deleteFileAfterUpload)
+        {
+            var filePath = demo.FilePath;
+
+            myLogger.LogInformation($"Trying to back up demo file ({filePath})");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                myLogger.LogWarning($"The file to be backed up does not exist: {filePath}");
+                throw new Exception($"The file to be backed up does not exist: {filePath}");
+            }
+
+            if (!myFtpClient.IsConnected)
+            {
+                myFtpClient.AutoConnect();
+            }
+
+            var remoteDemoFileName = $"{demo.Id}.dem";
+            var remoteFilePath = $"/csgo/{remoteDemoFileName}";
+
+            var successfulUpload = myFtpClient.UploadFile(filePath, remoteFilePath, FtpRemoteExists.Skip, false, FtpVerify.Retry);
+            if (successfulUpload.IsFailure())
+            {
+                myLogger.LogWarning($"Uploading the demo file ({remoteDemoFileName}) to the FTP failed.");
+                throw new Exception($"Uploading the demo file ({remoteDemoFileName}) to the FTP failed.");
+            }
+
+            myLogger.LogInformation($"Successfully backed up demo file {remoteDemoFileName}");
+
+            if (deleteFileAfterUpload)
+            {
+                myLogger.LogInformation($"Deleting backed up demo file {remoteDemoFileName}");
+                System.IO.File.Delete(filePath);
+            }
+        }
+
+        public void Dispose()
+        {
+            myFtpClient.Disconnect();
+        }
+    }
+}
