cb73eff4c18b554ea9d36afdf1f351c27de0842f
Sandy <michaelsandy@gmx.de>
2021-12-27 17:40:38 +0100

    Create small display layout

---
+++ frontend/lib/views/smallAppLayoutWidget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:no_cry_babies/state/appState.dart';
+import 'package:no_cry_babies/state/gameState.dart';
+import 'package:no_cry_babies/views/botsWidget.dart';
+import 'package:no_cry_babies/views/candidatesWidget.dart';
+import 'package:no_cry_babies/views/scrambleWidget.dart';
+import 'package:no_cry_babies/views/teamWidget.dart';
+
+class SmallAppLayoutWidget extends StatelessWidget {
+  const SmallAppLayoutWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: CandidatesWidget(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BotsWidget(),
+            ScrambleWidget(),
+          ],
+        ),
+        Expanded(
+          flex: 2,
+          child: StoreConnector<AppState, GameState>(
+            converter: (store) => store.state.gameState,
+            builder: (context, game) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: TeamWidget(
+                      imagePath: 'assets/t.png',
+                      team: game.t.players,
+                      name: "Terrorists",
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: TeamWidget(
+                      imagePath: 'assets/ct.png',
+                      team: game.ct.players,
+                      name: "Counter Terrorists",
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
