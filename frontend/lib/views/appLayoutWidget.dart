4ebf47ce908d899e84bee653a6057e3fc4973b18
Sandy <michaelsandy@gmx.de>
2021-12-27 18:10:04 +0100

    Improve small screen layout

--- frontend/lib/views/appLayoutWidget.dart
+++ frontend/lib/views/appLayoutWidget.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:no_cry_babies/views/largeAppLayoutWidget.dart';
 import 'package:no_cry_babies/views/smallAppLayoutWidget.dart';
 
+const int displayThreshold = 600;
+
 class AppLayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth > 600) {
+      if (constraints.maxWidth > displayThreshold) {
         return LargeAppLayoutWidget();
       }
       return SmallAppLayoutWidget();
