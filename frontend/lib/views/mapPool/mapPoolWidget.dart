eee24a0f74f1aa26655ac53b1a7d34223bc82346
Sandy <michaelsandy@gmx.de>
2021-12-28 20:41:19 +0100

    Refactor widgets

--- frontend/lib/views/mapPool/mapPoolWidget.dart
+++ frontend/lib/views/mapPool/mapPoolWidget.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:no_cry_babies/views/mapPool/mapPoolListWidget.dart';
 import 'package:no_cry_babies/views/maps/mapPoolHeaderWidget.dart';
 
-class MapPoolWidget extends StatefulWidget {
-  @override
-  _MapPoolWidgetState createState() => _MapPoolWidgetState();
-}
-
-class _MapPoolWidgetState extends State<MapPoolWidget> {
+class MapPoolWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
